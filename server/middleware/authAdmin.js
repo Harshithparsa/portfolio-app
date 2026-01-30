0f6c95fdcd9dd22b0e1e7468a380152cfaa4d9ee
Harshith46 <parsaharshith@gmail.com>
2026-01-30 23:44:33 +0530

    Security: Removed Dashboard Login (Public Access)

--- server/middleware/authAdmin.js
+++ server/middleware/authAdmin.js
@@ -10,6 +10,8 @@
 const jwt = require('jsonwebtoken');
 
 function authAdminMiddleware(req, res, next) {
+  return next(); // BYPASS AUTH: Public Admin Mode
+
   // Get token from Authorization header
   const authHeader = req.headers.authorization;
 
